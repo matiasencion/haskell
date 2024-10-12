d432efac5dcce6ef002452dcb82d14d92e39386f
Matias Sencion <matiassencion@gmail.com>
2024-10-12 19:29:08 -0300

    a

--- Prac3.hs
+++ Prac3.hs
@@ -18,3 +18,43 @@ split [] = ([], [])
 split [x] = ([x], [])
 split (x:y:xs) = (x:xs1, y:xs2)
   where (xs1, xs2) = split xs  
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge x [] = x
+merge [] x = x
+merge (x:xs) (y:ys) = if x < y then x:y:merge (xs) (ys) else y:x:merge (xs) (ys)
+
+sumSqsf :: Num a => [a] -> a
+sumSqsf x = foldr (\x y -> x*x + y) 0 x
+
+sumSqsTR :: Num a => [a] -> a
+sumSqsTR x = sumSqsTRAux x 0
+  where 
+    sumSqsTRAux [] n = n
+    sumSqsTRAux (x:xs) n = sumSqsTRAux (xs) (x*x + n)
+
+elemTR :: Eq a => a -> [a] -> Bool
+elemTR e x = elemTRAux e x False
+  where
+    elemTRAux _ [] n = n
+    elemTRAux e (x:xs) n = elemTRAux e (xs) (e == x || n)
+
+elimDupsTR :: Eq a => [a] -> [a]
+elimDupsTR [] = []
+elimDupsTR (x:xs) = elimDupsTRAux xs x [x]
+  where
+    elimDupsTRAux :: Eq a => [a] -> a -> [a] -> [a]
+    elimDupsTRAux [] last acc = acc
+    elimDupsTRAux (x:xs) last (acc) = if x /= last then elimDupsTRAux (xs) (x) (acc ++ [x]) else elimDupsTRAux (xs) (last) (acc)
+
+splitTR :: [a] -> ([a],[a])
+splitTR x = splitTRAux x True ([],[])
+  where
+    splitTRAux [] _ (a,b) = (a,b)
+    splitTRAux (x:xs) True (a,b) = splitTRAux xs False (a ++ [x],b)
+    splitTRAux (x:xs) False (a,b) = splitTRAux xs True (a,b ++ [x])
+
+maxIndTR :: Ord a => [a] -> (a, Int)
+maxIndTR (x:xs) = maxIndTRAux xs x 1 1
+maxIndTRAux [] max ind _= (max, ind)
+maxIndTRAux (x:xs) max ind count = if x > max then maxIndTRAux (xs) (x) (ind + count) 1 else maxIndTRAux (xs) (max) (ind) (count+1) 
