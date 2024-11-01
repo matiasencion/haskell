04a81bf41f90f92ca1eff24a2c7044753fa2c478
Matias Sencion <matiassencion@gmail.com>
2024-11-01 00:01:04 -0300

    a

--- lab-progfunc/Lintings.hs
+++ lab-progfunc/Lintings.hs
@@ -31,33 +31,53 @@ freeVariables (If expr1 expr2 expr3) = freeVariables (expr1) ++ freeVariables (e
 --------------------------------------------------------------------------------
 -- Reduce expresiones aritméticas/booleanas
 -- Construye sugerencias de la forma (LintCompCst e r)
-litToInt :: Lit -> Integer
-litToInt (LitInt a) = a
 
-litToBool :: Lit -> Bool
-litToBool (LitBool a) = a
+computeConstant :: Op -> Lit -> Lit -> (Expr, [LintSugg])
+computeConstant Add (LitInt n) (LitInt m)
+    | (n + m) >= 0 = (Lit (LitInt (n + m)), [LintCompCst (Infix Add (Lit (LitInt n)) (Lit (LitInt m))) (Lit (LitInt (n + m)))])
+    | otherwise = (Infix Add (Lit (LitInt n)) (Lit (LitInt m)), [])
 
+computeConstant Sub (LitInt n) (LitInt m)
+    | (n - m) >= 0 = (Lit (LitInt (n - m)), [LintCompCst (Infix Sub (Lit (LitInt n)) (Lit (LitInt m))) (Lit (LitInt (n - m)))])
+    | otherwise = (Infix Sub (Lit (LitInt n)) (Lit (LitInt m)), [])
 
-computeConstantOp :: Op -> Lit -> Lit -> [LintSugg]
-computeConstantOp (Add) (LitInt int1) (LitInt int2)
-    | (int1 + int2) >= 0 = [(LintCompCst (Infix (Add) (Lit (LitInt int1)) (Lit (LitInt int2))) (Lit (LitInt (int1 + int2))))]
-    | otherwise = []
-computeConstantOp (Sub) (LitInt int1) (LitInt int2)
-    | (int1 - int2) >= 0 = [(LintCompCst (Infix (Add) (Lit (LitInt int1)) (Lit (LitInt int2))) (Lit (LitInt (int1 - int2))))]
-    | otherwise = []
-computeConstantOp (Mult) (LitInt int1) (LitInt int2)
-    | (int1 * int2) >= 0 = [(LintCompCst (Infix (Add) (Lit (LitInt int1)) (Lit (LitInt int2))) (Lit (LitInt (int1 * int2))))]
-    | otherwise = []
-computeConstantOp (Div) (LitInt int1) (LitInt int2)
-    | (int2 /= 0) && (div int1 int2) >= 0 = [(LintCompCst (Infix (Add) (Lit (LitInt int1)) (Lit (LitInt int2))) (Lit (LitInt (div int1 int2))))]
-    | otherwise = []
+computeConstant Mult (LitInt n) (LitInt m)
+    | (n * m) >= 0 = (Lit (LitInt (n * m)), [LintCompCst (Infix Mult (Lit (LitInt n)) (Lit (LitInt m))) (Lit (LitInt (n * m)))])
+    | otherwise = (Infix Mult (Lit (LitInt n)) (Lit (LitInt m)), [])
+
+computeConstant Div (LitInt n) (LitInt m)
+    | (m /= 0) && (n `div` m) >= 0 = (Lit (LitInt (n `div` m)), [LintCompCst (Infix Div (Lit (LitInt n)) (Lit (LitInt m))) (Lit (LitInt (n `div` m)))])
+    | otherwise = (Infix Div (Lit (LitInt n)) (Lit (LitInt m)), [])
+
+computeConstant And (LitBool n) (LitBool m) = (Lit (LitBool (n && m)), [LintCompCst (Infix And (Lit (LitBool n)) (Lit (LitBool m))) (Lit (LitBool (n && m)))])
+
+computeConstant Or (LitBool n) (LitBool m) = (Lit (LitBool (n || m)), [LintCompCst (Infix And (Lit (LitBool n)) (Lit (LitBool m))) (Lit (LitBool (n || m)))])
 
 
 lintComputeConstant :: Linting Expr
-lintComputeConstant (Infix op (Lit lit1) (Lit lit2)) = ((Infix op (Lit lit1) (Lit lit2)), computeConstantOp(op, lit1, lit2))
-lintComputeConstant (Infix op expr1 expr2) = lintComputeConstant
+lintComputeConstant (Infix op (Lit lit1) (Lit lit2)) = computeConstant op lit1 lit2
+lintComputeConstant (Infix op expr1 expr2) = (Infix op newExpr1 newExpr2, sugg1 ++ sugg2)
+                                            where (newExpr1, sugg1) = lintComputeConstant expr1
+                                                  (newExpr2, sugg2) = lintComputeConstant expr2
 
+lintComputeConstant (App expr1 expr2) = (App newExpr1 newExpr2, sugg1 ++ sugg2)
+                                        where (newExpr1, sugg1) = lintComputeConstant expr1
+                                              (newExpr2, sugg2) = lintComputeConstant expr2
+                                        
+lintComputeConstant (Lam n expr) = (Lam n newExpr, sugg)
+                                        where (newExpr, sugg) = lintComputeConstant expr
 
+lintComputeConstant (Case expr1 expr2 (n1, n2, expr3)) = (Case newExpr1 newExpr2 (n1, n2, newExpr3), sugg1 ++ sugg2 ++ sugg3)
+                                                        where (newExpr1, sugg1) = lintComputeConstant expr1
+                                                              (newExpr2, sugg2) = lintComputeConstant expr2
+                                                              (newExpr3, sugg3) = lintComputeConstant expr3
+                    
+lintComputeConstant (If expr1 expr2 expr3) = (If newExpr1 newExpr2 newExpr3, sugg1 ++ sugg2 ++ sugg3)
+                                            where (newExpr1, sugg1) = lintComputeConstant expr1
+                                                  (newExpr2, sugg2) = lintComputeConstant expr2
+                                                  (newExpr3, sugg3) = lintComputeConstant expr3
+
+lintComputeConstant expr = (expre, [])
 
 --------------------------------------------------------------------------------
 -- Eliminación de chequeos redundantes de booleanos
@@ -67,8 +87,19 @@ lintComputeConstant (Infix op expr1 expr2) = lintComputeConstant
 -- Elimina chequeos de la forma e == True, True == e, e == False y False == e
 -- Construye sugerencias de la forma (LintBool e r)
 lintRedBool :: Linting Expr
-lintRedBool = undefined
+lintRedBool (Infix Eq (Lit (LitBool x)) expr)
+    | x = (expr, [LintBool (Infix Eq (Lit (LitBool x)) expr) expr])
+    | otherwise = (not expr, [LintBool (Infix Eq (Lit (LitBool x)) expr) (not expr)])
 
+lintRedBool (Infix Eq expr (Lit (LitBool x)))
+    | x = (expr, [LintBool expr (Infix Eq (Lit (LitBool x))) expr])
+    | otherwise = (not expr, [LintBool expr (Infix Eq (Lit (LitBool x))) (not expr)])
+
+lintRedBool (Infix op expr1 expr2) = (Infix op newExpr1 newExpr2, sugg1 ++ sugg2)
+                                    where (newExpr1, sugg1) = lintRedBool expr1
+                                          (newExpr2, sugg2) = lintRedBool expr2
+
+lintRedBool expr = (expr, [])
 
 --------------------------------------------------------------------------------
 -- Eliminación de if redundantes
