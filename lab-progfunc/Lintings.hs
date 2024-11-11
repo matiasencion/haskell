51d26be487155d2a958b3e961f2fa0bf07acc16c
Matias Sencion <matiassencion@gmail.com>
2024-11-11 16:33:08 -0300

    nuevos test

--- lab-progfunc/Lintings.hs
+++ lab-progfunc/Lintings.hs
@@ -15,7 +15,7 @@ freeVariables (Lit _) = []
 freeVariables (Infix _ expr1 expr2) = freeVariables (expr1) ++ freeVariables (expr2)
 freeVariables (App expr1 expr2) = freeVariables (expr1) ++ freeVariables (expr2)
 freeVariables (Lam x expr)  = [y | y <- freeVariables (expr), y /= x]
-freeVariables (Case expr1 expr2 (x, xs, expr3)) = freeVariables (expr1) ++ freeVariables (expr2) ++ [y | y <- freeVariables (expr3), y /= x && y /= x]
+freeVariables (Case expr1 expr2 (x, xs, expr3)) = freeVariables (expr1) ++ freeVariables (expr2) ++ [y | y <- freeVariables (expr3), y /= x && y /= xs]
 freeVariables (If expr1 expr2 expr3) = freeVariables (expr1) ++ freeVariables (expr2) ++ freeVariables (expr3)
 
 --------------------------------------------------------------------------------
@@ -51,11 +51,13 @@ computeConstant Div (LitInt n) (LitInt m)
 
 computeConstant And (LitBool n) (LitBool m) = (Lit (LitBool (n && m)), [LintCompCst (Infix And (Lit (LitBool n)) (Lit (LitBool m))) (Lit (LitBool (n && m)))])
 
-computeConstant Or (LitBool n) (LitBool m) = (Lit (LitBool (n || m)), [LintCompCst (Infix And (Lit (LitBool n)) (Lit (LitBool m))) (Lit (LitBool (n || m)))])
+computeConstant Or (LitBool n) (LitBool m) = (Lit (LitBool (n || m)), [LintCompCst (Infix Or (Lit (LitBool n)) (Lit (LitBool m))) (Lit (LitBool (n || m)))])
 
+computeConstant op lit1 lit2 = (Infix op (Lit lit1) (Lit lit2), [])
 
 lintComputeConstant :: Linting Expr
 lintComputeConstant (Infix op (Lit lit1) (Lit lit2)) = computeConstant op lit1 lit2
+
 lintComputeConstant (Infix op expr1 expr2) = (Infix op newExpr1 newExpr2, sugg1 ++ sugg2)
                                             where (newExpr1, sugg1) = lintComputeConstant expr1
                                                   (newExpr2, sugg2) = lintComputeConstant expr2
@@ -87,15 +89,17 @@ lintComputeConstant expr = (expr, [])
 -- Elimina chequeos de la forma e == True, True == e, e == False y False == e
 -- Construye sugerencias de la forma (LintBool e r)
 lintRedBool :: Linting Expr
-lintRedBool (Infix Eq (Lit (LitBool x)) expr)
-    | x = (newExpr, sugg ++ [LintBool (Infix Eq (Lit (LitBool x)) expr) newExpr])
-    | otherwise = (App (Var "not") newExpr, sugg ++ [LintBool (Infix Eq (Lit (LitBool x)) expr) (App (Var "not") newExpr)])
-        where (newExpr, sugg) = lintRedBool expr
+lintRedBool (Infix Eq expr (Lit (LitBool True))) = (newExpr, sugg ++ [LintBool (Infix Eq expr (Lit (LitBool True))) newExpr])
+                                                where (newExpr, sugg) = lintRedBool expr
 
-lintRedBool (Infix Eq expr (Lit (LitBool x)))
-    | x = (newExpr, sugg ++ [LintBool (Infix Eq expr (Lit (LitBool x))) newExpr])
-    | otherwise = (App (Var "not") newExpr, sugg ++ [LintBool (Infix Eq expr (Lit (LitBool x))) (App (Var "not") newExpr)])
-        where (newExpr, sugg) = lintRedBool expr
+lintRedBool (Infix Eq (Lit (LitBool True)) expr) = (newExpr, sugg ++ [LintBool (Infix Eq (Lit (LitBool True)) expr) newExpr])
+                                                where (newExpr, sugg) = lintRedBool expr
+
+lintRedBool (Infix Eq expr (Lit (LitBool False))) = (App (Var "not") newExpr, sugg ++ [LintBool (Infix Eq expr (Lit (LitBool False))) (App (Var "not") newExpr)])
+                                                where (newExpr, sugg) = lintRedBool expr
+
+lintRedBool (Infix Eq (Lit (LitBool False)) expr) = (App (Var "not") newExpr, sugg ++ [LintBool (Infix Eq (Lit (LitBool False)) expr) (App (Var "not") newExpr)])
+                                                where (newExpr, sugg) = lintRedBool expr
 
 lintRedBool (Infix op expr1 expr2) = (Infix op newExpr1 newExpr2, sugg1 ++ sugg2)
                                     where (newExpr1, sugg1) = lintRedBool expr1
@@ -296,9 +300,11 @@ lintAppend expr = (expr, [])
 -- Construye sugerencias de la forma (LintComp e r)
 
 lintComp :: Linting Expr -- !!!! CUIDADO CON (Var x) en el pattern matching, podria ser una Expr general
-lintComp (App expr1 (App expr2 (Var x))) = (App (Infix Comp newExpr1 newExpr2) (Var x), sugg1 ++ sugg2 ++ [LintComp (App expr1 (App expr2 (Var x))) (App (Infix Comp newExpr1 newExpr2) (Var x))]) 
-                                          where (newExpr1, sugg1) = lintComp expr1
-                                                (newExpr2, sugg2) = lintComp expr2
+--lintComp (App expr1 (App expr2 (Var x))) = (App (Infix Comp expr1 expr2) (Var x), [LintComp (App expr1 (App expr2 (Var x))) (App (Infix Comp expr1 expr2) (Var x))]) 
+
+--lintComp (App expr1 (App expr2 (Lit x))) = (App (Infix Comp expr1 expr2) (Lit x), [LintComp (App expr1 (App expr2 (Lit x))) (App (Infix Comp expr1 expr2) (Lit x))]) 
+
+lintComp (App expr1 (App expr2 expr3)) = (App (Infix Comp expr1 expr2) expr3, [LintComp (App expr1 (App expr2 expr3)) (App (Infix Comp expr1 expr2) expr3)]) 
 
 lintComp (Infix op expr1 expr2) = (Infix op newExpr1 newExpr2, sugg1 ++ sugg2)
                               where (newExpr1, sugg1) = lintComp expr1
@@ -367,7 +373,8 @@ lintEta expr = (expr, [])
 -- Sustituye recursión sobre listas por `map`
 -- Construye sugerencias de la forma (LintMap f r)
 lintMap :: Linting FunDef
-lintMap (FunDef f1 (Lam l1 (Case (Var l2) (Lit (LitNil)) (x, xs1, Infix Cons expr (App (Var f2) (Var xs2)))))) = if f1 == f2 && l1 == l2 && xs1 == xs2 then (FunDef f1 (App (Var "map") (Lam x expr)), [LintMap ((FunDef f1 (Lam l1 (Case (Var l2) (Lit (LitNil)) (x, xs1, (Infix Cons expr (App (Var f2) (Var xs2)))))))) (FunDef f1 (App (Var "map") (Lam x expr)))]) else ((FunDef f1 (Lam l1 (Case (Var l2) (Lit (LitNil)) (x, xs1, (Infix Cons expr (App (Var f2) (Var xs2))))))), [])
+lintMap (FunDef f1 (Lam l1 (Case (Var l2) (Lit (LitNil)) (x, xs1, Infix Cons expr (App (Var f2) (Var xs2)))))) = if f1 == f2 && l1 == l2 && xs1 == xs2 && not (elem f1 (freeVariables expr)) && not (elem xs1 (freeVariables expr)) && not (elem l1 (freeVariables expr))
+                                                                                                                  then (FunDef f1 (App (Var "map") (Lam x expr)), [LintMap ((FunDef f1 (Lam l1 (Case (Var l2) (Lit (LitNil)) (x, xs1, (Infix Cons expr (App (Var f2) (Var xs2)))))))) (FunDef f1 (App (Var "map") (Lam x expr)))]) else ((FunDef f1 (Lam l1 (Case (Var l2) (Lit (LitNil)) (x, xs1, (Infix Cons expr (App (Var f2) (Var xs2))))))), [])
 
 lintMap funcDef = (funcDef, [])
 
